a23b0e8791917dc1a7e1fc56b88e452442253a0b
Vo Si Luan <vosiluanse@gmail.com>
2020-06-09 19:58:52 +0700

    #2255

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var approvedPro_Controller = require("../Controller/ApprovedProduct_Controller");
+
+router.get('/ShowAprrovedProduct', function (req, res, next) {
+    approvedPro_Controller.loadSPDuocDuyet(req,res);
+});
+    router.get('/ShowProductKH', function (req, res, next) {
+    approvedPro_Controller.loadSPKH(req,res);
+});
+router.get('/ShowProductCty', function (req, res, next) {
+    approvedPro_Controller.loadSPCty(req,res);
+});
 module.exports = router;
