a11f6ec71293f782ea2949b65a778c3343e0fd36
PhanQuocPhu <60659261+PhanQuocPhu@users.noreply.github.com>
2020-06-09 20:42:09 +0700

    Edit aws

--- Controller/DangKyController.js
+++ Controller/DangKyController.js
@@ -1,10 +1,4 @@
-var AWS = require('aws-sdk');
-AWS.config.update({
-    region: "us-west-2",
-    accessKeyId: "AKIAIPKTQD44TJXEEIKQ",
-    secretAccessKey: "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",
-    endpoint: "https://dynamodb.us-west-2.amazonaws.com"
-});
+
 var docClient = new AWS.DynamoDB.DocumentClient();
 /* GET home page. */
 var CUSTOMEPOCH = 1300000000000; // artificial epoch
