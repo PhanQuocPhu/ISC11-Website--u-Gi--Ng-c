39c4e62d2ac5de3719031067206b061eebb4b3be
PhanQuocPhu <60659261+PhanQuocPhu@users.noreply.github.com>
2020-06-09 20:41:47 +0700

    Edit aws

--- Controller/LoginController.js
+++ Controller/LoginController.js
@@ -1,11 +1,4 @@
 
-var AWS = require('aws-sdk');
-AWS.config.update({
-    region: "us-west-2",
-    accessKeyId: "AKIAIPKTQD44TJXEEIKQ",
-    secretAccessKey: "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",
-    endpoint: "https://dynamodb.us-west-2.amazonaws.com"
-});
 var docClient = new AWS.DynamoDB.DocumentClient();
 /* GET home page. */
 exports.Login = function (req, res, ten,pwd) {
